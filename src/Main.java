a40ae283cc072a3d87fe145ba334d2cdeaf58c19
AndreyShl <andrejslapkov30@gmail.com>
2024-09-09 22:19:44 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-      System.out.printf("Привет это Дз1");
+      System.out.printf("Привет это уже Дз1");
     }
 }
