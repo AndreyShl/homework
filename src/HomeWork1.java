35c4df891f81745b25d25199d7a95c1a145e6cfe
AndreyShl <andrejslapkov30@gmail.com>
2024-09-10 19:33:41 +0300

    update File

--- src/HomeWork1.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class HomeWork1 {
-    public static void main(String[] args) {
-      System.out.printf("Привет Дз1!");
-    }
-}
