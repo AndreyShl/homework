4d0507c6bec9aaae6368420a4f7fb98bc894477d
AndreyShl <andrejslapkov30@gmail.com>
2024-12-18 21:18:26 +0300

    исправил стрим и обрезал последний элемент

--- src/homeworkStream/Main.java
+++ src/homeworkStream/Main.java
@@ -15,32 +15,34 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-
 public class Main {
     public static void main(String[] args) {
         AtomicInteger counter = new AtomicInteger();
-        System.out.println();
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
         System.out.println("начальный список: " + myList);
         myList.stream().
                 filter(str -> str.matches("\\w[^3]")).
                 sorted((str1, str2) -> {
-                    String firstIndex1 = str1.substring(1,2);
-                    String firstIndex2 = str2.substring(1,2);
+                    String firstIndex1 = str1.substring(1, 2);
+                    String firstIndex2 = str2.substring(1, 2);
                     return firstIndex1.compareTo(firstIndex2);
                 })
                 .sorted((str1, str2) -> {
-                    String firstIndex1 = str1.substring(0,1);
-                    String firstIndex2 = str2.substring(0,1);
+                    String firstIndex1 = str1.substring(0, 1);
+                    String firstIndex2 = str2.substring(0, 1);
                     return firstIndex2.compareTo(firstIndex1);
                 }).skip(1).
-                forEach(str -> {System.out.print(str.toUpperCase() + " ");
+                limit(myList.stream().filter(str -> str.matches("\\w[^3]")).count() - 2).
+                forEach(str -> {
+                    System.out.print(str.toUpperCase() + " ");
                     counter.getAndIncrement();
                 });
+
         System.out.println();
-        System.out.println("Конечное количество элементов: "+counter);
+        System.out.println("Конечное количество элементов: " + counter);
 
 
     }
+
 
 }
